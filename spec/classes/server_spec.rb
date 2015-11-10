146c9ad5677c7c47641024a3a4bf14f8acd91a6f
Chris Tessmer <chris.tessmer@onyxpoint.com>
2015-11-10 14:49:54 -0500

    (SIMP-604) Migrate to simplib and simpcat
    Before this commit, common SIMP-related custom functions were been kept
    in either `simp-common` or `simp-functions`, and the `concat` function
    was provided by the `simp-concat` module.  These functions are now
    provides by the `simp-simplib` and `simp-simpcat` modules, respectively.
    
    This update replaces all requirements in the RPM spec file,
    metadata.json, or .fixtures.yml to reflect the migration to the new
    modules.
    
    SIMP-601 #comment Migrated `pupmod-simp-vnc`.
    SIMP-604 #comment Updated `pupmod-simp-vnc`.
    SIMP-180 #comment Fixed broken spec (Travis) tests in `pupmod-simp-vnc`
    
    Change-Id: I61326e3511ab9b839a9d45e9a799fdfc1b0908a8

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -1,42 +1,46 @@
 require 'spec_helper'
 
 describe 'vnc::server' do
+  context  'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) do
+        facts
+      end
 
-  let(:facts) {{
-    :operatingsystem => 'RedHat',
-    :lsbmajdistrelease => '6'
-  }}
+      context "on #{os}" do
+        it { should create_class('vnc::server') }
+        it { should compile.with_all_deps }
 
-  it { should create_class('vnc::server') }
-  it { should compile.with_all_deps }
+        it { should contain_class('common') }
+        it { should contain_class('xinetd') }
+        it { should contain_class('xwindows') }
 
-  it { should contain_class('common') }
-  it { should contain_class('xinetd') }
-  it { should contain_class('xwindows') }
+        it do
+          should create_vnc__server__create('vnc_standard').with({
+            'port'      => '5901',
+            'geometry'  => '1024x768',
+            'depth'     => '16'
+          })
+        end
+        it do
+          should create_vnc__server__create('vnc_lowres').with({
+            'port'      => '5902',
+            'geometry'  => '800x600',
+            'depth'     => '16'
+          })
+        end
+        it do
+          should create_vnc__server__create('vnc_highres').with({
+            'port'      => '5903',
+            'geometry'  => '1280x1024',
+            'depth'     => '16'
+          })
+        end
 
-  it do
-    should create_vnc__server__create('vnc_standard').with({
-      'port'      => '5901',
-      'geometry'  => '1024x768',
-      'depth'     => '16'
-    })
+        it { should contain_package('tigervnc-server') }
+        it { should create_xwindows__gdm__set('enable_xdmcp') }
+
+      end
+    end
   end
-  it do
-    should create_vnc__server__create('vnc_lowres').with({
-      'port'      => '5902',
-      'geometry'  => '800x600',
-      'depth'     => '16'
-    })
-  end
-  it do
-    should create_vnc__server__create('vnc_highres').with({
-      'port'      => '5903',
-      'geometry'  => '1280x1024',
-      'depth'     => '16'
-    })
-  end
-
-  it { should contain_package('tigervnc-server') }
-  it { should create_xwindows__gdm__set('enable_xdmcp') }
-
 end

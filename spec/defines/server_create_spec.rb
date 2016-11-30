6c0462e94378e6be9078c516278b29fe32d06474
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2016-11-30 18:20:47 +0000

    (SIMP-2123) Fix Travis build for master branches (#16)
    SIMP-2127 #close
    SIMP-2123 #comment

--- spec/defines/server_create_spec.rb
+++ spec/defines/server_create_spec.rb
@@ -1,30 +1,38 @@
 require 'spec_helper'
 
 describe 'vnc::server::create' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
 
-  let(:title) { 'awesome_vnc' }
+        let(:title) { 'awesome_vnc' }
 
-  let(:params) { {:port => '5900', :geometry => '1280x1024', :depth => '16', :screensaver_timeout => '15'} }
+        let(:params) { {:port => '5900', :geometry => '1280x1024', :depth => '16', :screensaver_timeout => '15'} }
 
-  it { should compile.with_all_deps }
+        it { should compile.with_all_deps }
 
-  it { should contain_class('xinetd') }
+        it { should contain_class('xinetd') }
 
-  it do
-    should contain_xinetd__service('awesome_vnc').with({
-      'flags'          => 'REUSE',
-      'protocol'       => 'tcp',
-      'socket_type'    => 'stream',
-      'x_wait'         => 'no',
-      'x_type'         => 'UNLISTED',
-      'log_on_success' => 'HOST PID DURATION',
-      'user'           => 'nobody',
-      'server'         => '/usr/bin/Xvnc',
-      'server_args'    => "-inetd -localhost -audit 4 -s 15 -query localhost -NeverShared -once -SecurityTypes None -desktop awesome_vnc -geometry 1280x1024 -depth 16",
-      'disable'        => 'no',
-      'only_from'      => '127.0.0.1',
-      'port'           => 5900
-    })
+        it do
+          should contain_xinetd__service('awesome_vnc').with({
+            'flags'          => 'REUSE',
+            'protocol'       => 'tcp',
+            'socket_type'    => 'stream',
+            'x_wait'         => 'no',
+            'x_type'         => 'UNLISTED',
+            'log_on_success' => 'HOST PID DURATION',
+            'user'           => 'nobody',
+            'server'         => '/usr/bin/Xvnc',
+            'server_args'    => "-inetd -localhost -audit 4 -s 15 -query localhost -NeverShared -once -SecurityTypes None -desktop awesome_vnc -geometry 1280x1024 -depth 16",
+            'disable'        => 'no',
+            'only_from'      => '127.0.0.1',
+            'port'           => 5900
+          })
+        end
+      end
+    end
   end
-
 end
